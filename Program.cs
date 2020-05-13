15b3c42902ec9c5a1144b2731f60bdf458ccf843
cymsu <61240120+cymsu@users.noreply.github.com>
2020-05-13 22:53:24 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kurs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             /*string characterName = "John";
+             int characterAge;
+             characterAge = 35;
+
+             Console.WriteLine("There was a man named " + characterName);
+             Console.WriteLine("He was " + characterAge + " years old");        
+             Console.WriteLine("He liked the name " + characterName);
+             Console.WriteLine("But he didn't liked he was " + +characterAge);
+
+
+
+             Console.ReadLine();
+             */
+
+
+
+            /* string phrase = "World";
+             char grade = 'a';
+             int age = 30;
+             double gpa = 3.3;
+             bool isMale = true;
+
+
+             Console.WriteLine(30);
+
+
+
+             Console.ReadLine();
+             */
+
+            /*
+            string phrase = "Avatar";
+            Console.WriteLine("Rate the " + phrase + " movie");
+            Console.WriteLine(phrase.Substring (0, 5 ));
+            Console.WriteLine(phrase.IndexOf('v');
+            Console.ReadLine();
+            */
+
+            g
+
+
+        }
+    }
+}
